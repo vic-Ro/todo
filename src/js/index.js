2a8eb8af1246e23aca3fe9f54d5b38a96b15b04a
Vic Romero <vromerocomino@gmail.com>
2020-12-08 17:34:45 +0100

    Forms html and css clearer

--- src/js/index.js
+++ src/js/index.js
@@ -11,50 +11,50 @@ import {
 import addToDoButtonListener from './modules/todos-form-listener';
 import validateText from './modules/validate';
 
-const projects = [];
-projects.push(createProject('Proyecto 1', 'Este es el proyecto 1'));
-projects.push(createProject('Proyecto 2', 'Este es el proyecto 2'));
-projects.push(createProject('Proyecto 3', 'Este es el proyecto 3'));
-projects.push(createProject('Proyecto 4', 'Este es el proyecto 4'));
-projects[0].addTasks(
-  createToDos('Todo 1', 'Este es el todo 1', '24h', 'priority1'),
-);
-projects[0].addTasks(
-  createToDos('Todo 2', 'Este es el todo 2', '24h', 'priority1'),
-);
-projects[0].addTasks(
-  createToDos('Todo 3', 'Este es el todo 3', '24h', 'priority2'),
-);
-projects[0].addTasks(
-  createToDos('Todo 4', 'Este es el todo 4', '24h', 'priority3'),
-);
-projects[0].removeTask(1);
-renderToDos(projects[0]);
-renderAllProjects(projects);
+// const projects = [];
+// projects.push(createProject('Proyecto 1', 'Este es el proyecto 1'));
+// projects.push(createProject('Proyecto 2', 'Este es el proyecto 2'));
+// projects.push(createProject('Proyecto 3', 'Este es el proyecto 3'));
+// projects.push(createProject('Proyecto 4', 'Este es el proyecto 4'));
+// projects[0].addTasks(
+//   createToDos('Todo 1', 'Este es el todo 1', '24h', 'priority1'),
+// );
+// projects[0].addTasks(
+//   createToDos('Todo 2', 'Este es el todo 2', '24h', 'priority1'),
+// );
+// projects[0].addTasks(
+//   createToDos('Todo 3', 'Este es el todo 3', '24h', 'priority2'),
+// );
+// projects[0].addTasks(
+//   createToDos('Todo 4', 'Este es el todo 4', '24h', 'priority3'),
+// );
+// projects[0].removeTask(1);
+// renderToDos(projects[0]);
+// renderAllProjects(projects);
 
-const form = document.getElementById('todos-form');
-form.addEventListener('click', (e) => {
-  if (e.target.classList.contains('button--cancel')) {
-    e.preventDefault();
-    form.classList.add('hidden');
-    if (form.lastChild.classList.contains('error-msg')) {
-      form.lastChild.remove();
-    }
-  }
-  if (e.target.classList.contains('button--save')) {
-    e.preventDefault();
-    if (
-      // eslint-disable-next-line operator-linebreak
-      validateText(form.firstChild.value, form, 20, 'Name') &&
-      validateText(form.children[1].value, form, 40, 'Description')
-    ) {
-      addToDo(projects[0]);
-      form.reset();
-      form.classList.toggle('hidden');
-    }
-  }
-});
-//* OK
-projectFormListener(projects);
-addProjectButtonListener();
-addToDoButtonListener();
+// const form = document.getElementById('todos-form');
+// form.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('button--cancel')) {
+//     e.preventDefault();
+//     form.classList.add('hidden');
+//     if (form.lastChild.classList.contains('error-msg')) {
+//       form.lastChild.remove();
+//     }
+//   }
+//   if (e.target.classList.contains('button--save')) {
+//     e.preventDefault();
+//     if (
+//       // eslint-disable-next-line operator-linebreak
+//       validateText(form.firstChild.value, form, 20, 'Name') &&
+//       validateText(form.children[1].value, form, 40, 'Description')
+//     ) {
+//       addToDo(projects[0]);
+//       form.reset();
+//       form.classList.toggle('hidden');
+//     }
+//   }
+// });
+// //* OK
+// projectFormListener(projects);
+// addProjectButtonListener();
+// addToDoButtonListener();
