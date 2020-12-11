c4097bc66a76d6b96b9f008f9a5e65bcba6ba0e7
Vic Romero <vromerocomino@gmail.com>
2020-12-11 14:29:18 +0100

    Edit project feature

--- src/js/modules/element-generator.js
+++
@@ -1,9 +0,0 @@
-const generateElement = (element, ...styles) => {
-  const elementTemplate = document.createElement(`${element}`);
-  [...styles].forEach((cssStyle) => {
-    elementTemplate.classList.add(cssStyle);
-  });
-  return elementTemplate;
-};
-
-export default generateElement;
